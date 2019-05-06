2e83a2e35171b86aa0b1b8ae83dcccf13bd4c31e
KMolina <kevenmolina@gmail.com>
2019-05-06 12:41:29 -0500

    added image asstets and styling mockup

--- script/script.js
+++ script/script.js
@@ -1,17 +1,22 @@
-// alert("it works");
+alert("it works");
 //Treating this as a rough draft
 
 /*---Constants---*/
 var players;
 /*----app's state(variable)----*/
-var turn,  winner, score, playerHand,computerHand
+var turn,  winner, score, playerHand,computerHand;
 
 /*---cached elements references---*/
 //count of player card pile to determine winner.
 
 /*---event listener---*/
 //player clicking their card.
+document.getElementById('player').addEventListener('click', cardFlip);
+var playerCardT=document.getElementById('p1-card').addEventListener();;
+var cpuCardT=document.getElementById('cpu-card').addEventListener();
 //player clikcing  the reset button.
+document.getElementsByTagName('button').addEventListener('click',init);
+document.getElementById('table').addEventListener() 
 //player clicking start button/ note: DOM manipulation could allow them both to be the same.
 /*---functions---*/
 //"shuffle card deck.", assign cards to each player "randomly".
@@ -30,15 +35,12 @@ function init(){
         player: 0,
         cpu: 0,
         tie : 0,
-    } //currently keeping track of the score....
-
-    //an idea for the assigning of suites is  assigning a range of cards to a suit
-   
+    } //Winner is determined by the greater total of pile
         /*Taking in to account suites are largly ignored. 
-        The number/rank is what determines the winner*/
-
-        //Have 52 cards in deck
-        //shuffle deck
+        refer to rps on assigning image/value
+        */
+        
+        
         function shuffle (a){
             a.sort(() => Math.random()-0.5);
             return a
@@ -47,11 +49,21 @@ function init(){
         cards(cpuHand);
 }
 //WIP
-function cardFlip(evt.target){
+function cardFlip(){
+      table.push()
+
+}//on click, array element is pushed to table, while the player shifts elements.
+//prompt's cpu to do the same 
+var table = function cardJudge(a,b){
+    if(a>b){
         
+    }
+};
 
+checkForWinner(){
+    //conditional that  activates once there are no more cards
+    //evaluates who has the greater total  between players
 }
+// function render (){
 
-function render (){
-
-}
+// }
