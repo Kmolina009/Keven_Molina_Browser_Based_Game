15c4a879a4b272e72fa1a26cb1829f15dedd1dd0
KMolina <kevenmolina@gmail.com>
2019-05-09 17:26:00 -0500

    js features have added functionalliy, score keepers removed

--- script/script.js
+++ script/script.js
@@ -1,108 +1,156 @@
-
 //Main Goal: Link the events together
-
+//Make sure to get back to your CSS
 /*---Constants---*/
 var players;
+
 /*----app's state(variable)----*/
-var turn,  winner, score, playerHand, cpuHand, playerPile, cpuPile;
+var winner, score, playerHand,cpuHand,playerPile,cpuPile,table
+/*---cached elements references---*/
+
 playerHand= [1,2,3,4,5,6,7,8,9,10,11,12,13,14,
-    1,2,3,4,5,6,7,8,9,10,11,12,13,14,];
+                    1,2,3,4,5,6,7,8,9,10,11,12,13,14,];
+    
+
 playerPile=[];
 cpuHand = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,
 1,2,3,4,5,6,7,8,9,10,11,12,13,14];
 cpuPile=[];
-var table = {
-    cards : [],
-    judge:function(){ if(this.cards.length === 2){
-            cardJudge(table.cards);
-    }
-        }
-            }// table, if two values are present, function judge}
-console.log(playerHand);
-console.log(playerPile);
-console.log(cpuHand);
-console.log(cpuPile);
-console.log(table.cards);
-/*---cached elements references---*/
-//count of player card pile to determine winner.
 
+table = {
+cards: [],
+judge:function(){ if(this.cards.length === 2){
+cardJudge(table.cards)()
+        }  
+    }
+};
+
+//count of player card pile to determine winner.
 /*---event listener---*/
 //player clicking their card.
-document.getElementsById("player").addEventListener("click", cons/*cardFlip(playerHand)*/);//player click event
-// var playerCardT = document.getElementById('p1-card').addEventListener();//player card on table, should load coresponding card image
-// var cpuCardT=document.getElementById('cpu-card').addEventListener();//computer card on table
-// document.getElementsById('reset').addEventListener('click',init);//reset button
+
+document.getElementById('player').addEventListener('click',function(){
+    cardFlip(playerHand)
+    })
+
+var playerCardT=document.getElementById('p1-card').addEventListener();//player card on table
+
+var cpuCardT=document.getElementById('cpu-card').addEventListener();//computer card on table
+
+document.getElementsByTagName('button').addEventListener('click',init);//reset button
+
+// var table = document.getElementById('table').addEventListener(onload,cardJudge)// table, if two values are present, function judge
+
 //player clicking start button/ note: DOM manipulation could allow them both to be the same.
 /*---functions---*/
+//"shuffle card deck.", assign cards to each player "randomly".
 
-//check for winner(could trigger once there are no cards left)
-
-init();
-
-shuffle(playerHand);
-shuffle(cpuHand);
-
-
-    
-//WIP
-function cardFlip(i){
+function cardFlip(arr){
     // Element to table.
     //cpu sends element to table.
-    table.cards.push(i[0]);
-    if(table.cards.push){
-    }
-     return table
-};
- //cpu sends element to table.but how can I get two elements sent
-cardFlip(cpuHand);
-
-//The hands at play lose there current card
-function playedCard(i) {
-    i.splice(0,1);
-	return i
-  };
-  
-  function cardJudge(){  //will need testing
+    arr.sort(() => Math.random()-0.5);
+    table.cards.push(arr[0]);
+     arr.splice(0,1);
+     cpuFlip()
+     return table.cards
+ };
+ function cpuFlip(){
+     if(table.cards.length===1){
+        cpuHand.sort(() => Math.random()-0.5);
+    table.cards.push(cpuHand.pop());
+    cardJudge(table);
+     return table.cards
+     
+     }}
+ 
+ //The hands at play lose there current card
+// function playedCard(i) {
+//     i.splice(0,1);
+//      return i
+//   };
+ function cardJudge(table){  //will need testing
     //activates once there are two cards on table
-    
-    if(a[0]>a[1]){
-        playerPile.push(a[0],a[1])
-    } else {cpuPile.push(a[0],a[1])
-    return a
+    if(table.cards[0]>table.cards[1]){
+        playerPile.push(table.cards[0],table.cards[1])
+    } else {cpuPile.push(table.cards[0],table.cards[1])
     }
-};
-//use reduce method to "tally up" card value
+    table.cards.splice(0,2)
+    return table
+ };
 
-function checkForWinner(){
+ checkForWinner = function(){
     if(playerHand.length===0&&cpuHand.length===0){
-
-    }
+        cpuPile.reduce((acc,currentval)=> acc+currentval);
+        playerPile.reduce(total,num);
+        getWinner();
+    }    
+ }
     /*conditional that  activates once there are no more cards in either players hand
-    */if(playerPile>cpuPile){
-        console.log("You win")
+    
+*/
+ function  getWinner(){
+    if(playerPile>cpuPile){
+        alert("You Win")
     }else if(cpuPile>playerPile){
-        console.log("Try Again")
+        alert("Try Again")
     } else{
-        "It's a draw"
+        alert("It's a Draw");
     }
     //evaluates who has the greater total sum between players
 };
+
+
+
+// playedCard(cardFlip());
+// playedCard(cardFlip());
+
+init ();
+
+function init(){
+    shuffle(playerHand);
+    shuffle(cpuHand);
+
+    players= {
+        player: 0,
+        cpu: 0,
+        tie : 0,
+    }
+           };
+           
+           console.log(cpuFlip);
+ console.log(cpuHand);
+console.log(cpuPile);
+console.log(table.cards);
+console.log(playerHand);
+console.log(playerPile);// table, if two values are present, function judge}
+    //Winner is determined by the greater total of pile
+
+       
+/*Another version that involves pushing things to the table
+That prompts the cpu to push
+function cardFlip(i){
+   // Element to table.
+   //cpu sends element to table.
+   table.cards.push(i[0]);
+   if(table.cards.push){
+   }
+    return table
+};
+*/
+      
+
+//WIP
+
+
+
+
+
+
+ 
+//use reduce method to "tally" card value
+
+
+   //evaluates who has the greater total sum between players
+
 // function render (){
 
-    function init(){
-        players= {
-            player: 0,
-            cpu: 0,
-            tie : 0,
-          
-        } //Winner is determined by the greater total of pile
-            /*Taking in to account suites are largly ignored. 
-            refer to rps on assigning image/value
-            */
-           table.cards=[];
-    }
-    function shuffle (a){
-        a.sort(() => Math.random()-0.5);
-        return a
-        render()
-    };
+// }
